c084c1dcdc2a9636931be91217a17e833297bc18
wonkwh <wonkwh@gmail.com>
2022-04-04 20:37:09 +0900

    ♻️ refactor using hook

--- App.js
+++ App.js
@@ -3,47 +3,26 @@ import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { Asset } from 'expo-asset';
+import { Asset, useAssets } from 'expo-asset';
 import  * as Font  from 'expo-font';
 import Ionicons from '@expo/vector-icons/Ionicons'
 
-const loadFonts = (fonts) => fonts.map( (font) => Font.loadAsync(font));
-
-const loadImages = (images) => images.map( (image) => {
-  if (typeof image === "string") {
-    return Image.prefetch(image);
-  } else {
-    return Asset.loadAsync(image);
-  }
-});
-
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font])
-    const images = loadImages([
-      require("./my_profile.jpeg"),
-      "https://reactnative.dev/img/oss_logo.png"
-    ])
+  //use Hooks
+  const [assets] = useAssets([require("./my_profile.jpeg")])
+  const [loaded] = Font.useFonts(Ionicons.font)
 
-    await Promise.all([...fonts, ...images])
-  };
-
-  if (!ready) { 
-    return (<AppLoading 
-      startAsync={ startLoading }
-      onFinish={onFinish}
-      onError ={console.error}
-    />)
-  } else {
-    return (
-      <View style={styles.container}>
-        <Text>App Loading is Done</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
+  if (!assets || !loaded) { 
+    return <AppLoading />;
+  } 
+  
+  return (
+    <View style={styles.container}>
+      <Text>App Loading is Done!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+  
 }
 
 const styles = StyleSheet.create({
